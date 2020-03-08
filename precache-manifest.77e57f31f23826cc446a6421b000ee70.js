55198de793b2eba10432f572865c46ccd46d5d54
sergey <paksergey94@gmail.com>
2020-03-08 14:10:45 +1000

    Updates

---
+++ precache-manifest.77e57f31f23826cc446a6421b000ee70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6d0936447be2b74fcaee006c4809fd9",
+    "url": "/memoriy-game-react-redux/index.html"
+  },
+  {
+    "revision": "0ea3d2fceba01fdba3b3",
+    "url": "/memoriy-game-react-redux/static/css/main.3cff9986.chunk.css"
+  },
+  {
+    "revision": "6cbcf430c551e734f300",
+    "url": "/memoriy-game-react-redux/static/js/2.a0d9c879.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/memoriy-game-react-redux/static/js/2.a0d9c879.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ea3d2fceba01fdba3b3",
+    "url": "/memoriy-game-react-redux/static/js/main.77725ad0.chunk.js"
+  },
+  {
+    "revision": "52a4a8cd5e77fecdf027",
+    "url": "/memoriy-game-react-redux/static/js/runtime-main.4584dba6.js"
+  }
+]);
